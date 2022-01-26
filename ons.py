d8f5eb2654172ad5de9e1f6e1dbb005463db5751
Frankie Barr <frankiebarr2@hotmail.co.uk>
2022-01-26 15:37:58 +0000

    made it work for 1 dataset...

--- ons.py
+++ ons.py
@@ -55,20 +55,23 @@ def get_available_datasets():
     return df
 
 
-def get_dataset(datasets, title):
+def get_dataset(datasets, title, choice=None):
     dataset = datasets.loc[datasets["title"].str.contains(title, case=False, regex=False)]
     
     if dataset.shape[0] > 1:
         logger.info(f"{dataset.shape[0]} datasets found.")
-        print(f'{dataset["title"]}')
-        choice = int(input("Choose index of desired dataset: "))
-        return dataset.loc[choice]
+        if choice:
+            return dataset.loc[choice]
+        else:
+            print(f'{dataset["title"]}')
+            choice = int(input("Choose index of desired dataset: "))
+            return dataset.loc[choice]
 
     return dataset
 
 
 def get_latest_version(dataset):
-    links = dict(dataset["links"][8])
+    links = dict(dataset["links"])
     url = links["latest_version"]["href"]
 
     return url
@@ -86,18 +89,44 @@ def get_dataset_dimensions(url):
         r_2 = requests.get(options_url).json()
 
         options = {option["label"]: option["option"] for option in r_2["items"]}
-        dimensions[dimension_label] = options
+        dimensions[dimension_id] = options
     return dimensions
                                                                                                                                                                                                                                                       
 
+def get_data(url, dimensions):
+    r = requests.get(f"{url}/observations", params=dimensions).json()
+    with open("output/firstresults.json", "w") as file:
+        json.dump(r, file, indent=4)
+    # df = pd.DataFrame(r)
+    # return df
 
 
 if __name__ == "__main__":
     
     datasets = get_available_datasets()
 
-    dataset = get_dataset(datasets, "traffic")
+    dataset = get_dataset(datasets, "house", 25)
 
     latest_url = get_latest_version(dataset)
+    print(latest_url)
+    pprint(get_dataset_dimensions(latest_url))
 
-    print(get_dataset_dimensions(latest_url))
+    dimensions = {
+        "buildstatus": "all",
+        "geography": "E06000009",
+        "housesalesandprices": "mean",
+        "month": "jun",
+        "propertytype": "all",
+        "time": "*"
+    }
+
+    # dimensions = {
+    #     "daymonth": "12-12",
+    #     "geography": "K02000001",
+    #     "pedestriansandvehicles": "cars",
+    #     "seasonaladjustment": "non-seasonal-adjustment",
+    #     "time": "*",
+    #     "trafficcameraarea": "london"
+    # }
+
+    df = get_data(latest_url, dimensions)
